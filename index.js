20a9a6d88a3c7d6dd7055448d91ca34e319321ed
lou <email": null>
2016-03-23 19:41:10 +1300

    Update index

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+// console.log("welcome to goe")
+
+// var h = require('hyperscript');
+// var $ = require("jquery");
+
+
+// window.setInterval(refreshMsg, 5000)
+// function refreshMsg(){
+//   $('div.message').remove()
+//   console.log("Time : ")
+//   getMessages()
+// }
+
+// function getMessages() {
+//   $.get("http://localhost:3000/v1/messages")
+//   .done(function(data){
+//     oldMessages = data.messages.length;
+//     for (var i = 0; i < data.messages.length; i++){
+//       console.log("data : ", data.messages +" : " + "old" + oldMessages)
+//       var sender = data.messages[i].sender;
+//       var message = data.messages[i].message;
+//       var html = h('div.message', {style: {'font-size': '1.5em'}}, h('p', {}, sender + ": " + message), h('hr',{}))
+//       $('main').append(html)
+//     }
+//   })
+//   .fail(function(err){
+//     console.log(err);
+//   })
+// }
+
+// $(document).ready(function(){
+//   $('form').submit(function(event){
+//     var newMessage = {}
+//     newMessage["sender"] = $('input#name').val()
+//     newMessage["message"] = $('textarea#message').val() //this was input#message
+//     $.post("http://localhost:3000/v1/messages", newMessage)// function(){
+//     // })
+//     // .done(function() {
+//     //   console.log('new message sent!')
+//     // })
+//     // .fail(function() {
+//     //   console.log( 'error' );
+//     // })
+// })
+// })
+
 console.log("welcome to goe")
 
 var h = require('hyperscript');
@@ -7,7 +53,7 @@ var $ = require("jquery");
 window.setInterval(refreshMsg, 5000)
 function refreshMsg(){
   $('div.message').remove()
-  console.log("Time :")
+  console.log("Time : " )
   getMessages()
 }
 
@@ -15,8 +61,8 @@ function getMessages() {
   $.get("http://localhost:3000/v1/messages")
   .done(function(data){
     oldMessages = data.messages.length;
-  for (var i = 0; i < data.messages.length; i++){
-      console.log("data", data.messages.length +" : " + "old" + oldMessages)
+    for (var i = 0; i < data.messages.length; i++){
+      console.log("data : ", data.messages +" : " + "old" + oldMessages)
       var sender = data.messages[i].sender;
       var message = data.messages[i].message;
       var html = h('div.message', {style: {'font-size': '1.5em'}}, h('p', {}, sender + ": " + message), h('hr',{}))
@@ -33,8 +79,8 @@ $(document).ready(function(){
     var newMessage = {}
     newMessage["sender"] = $('input#name').val()
     newMessage["message"] = $('textarea#message').val() //this was input#message
+    // maybe here need to push new message onto messages array
     $.post("http://localhost:3000/v1/messages", newMessage)// function(){
-
     // })
     // .done(function() {
     //   console.log('new message sent!')
@@ -44,3 +90,4 @@ $(document).ready(function(){
     // })
 })
 })
+
