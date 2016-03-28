36565406a217a01c37048c3296f44a1f50c9f013
lou <email": null>
2016-03-28 21:52:55 +1300

    Add fs promises function

--- index.js
+++ index.js
@@ -1,49 +1,3 @@
-// console.log("welcome to goe")
-
-// var h = require('hyperscript');
-// var $ = require("jquery");
-
-
-// window.setInterval(refreshMsg, 5000)
-// function refreshMsg(){
-//   $('div.message').remove()
-//   console.log("Time : ")
-//   getMessages()
-// }
-
-// function getMessages() {
-//   $.get("http://localhost:3000/v1/messages")
-//   .done(function(data){
-//     oldMessages = data.messages.length;
-//     for (var i = 0; i < data.messages.length; i++){
-//       console.log("data : ", data.messages +" : " + "old" + oldMessages)
-//       var sender = data.messages[i].sender;
-//       var message = data.messages[i].message;
-//       var html = h('div.message', {style: {'font-size': '1.5em'}}, h('p', {}, sender + ": " + message), h('hr',{}))
-//       $('main').append(html)
-//     }
-//   })
-//   .fail(function(err){
-//     console.log(err);
-//   })
-// }
-
-// $(document).ready(function(){
-//   $('form').submit(function(event){
-//     var newMessage = {}
-//     newMessage["sender"] = $('input#name').val()
-//     newMessage["message"] = $('textarea#message').val() //this was input#message
-//     $.post("http://localhost:3000/v1/messages", newMessage)// function(){
-//     // })
-//     // .done(function() {
-//     //   console.log('new message sent!')
-//     // })
-//     // .fail(function() {
-//     //   console.log( 'error' );
-//     // })
-// })
-// })
-
 console.log("welcome to goe")
 
 var h = require('hyperscript');
@@ -92,4 +46,63 @@ $(document).ready(function(){
     // })
 })
 })
+fs.readFileAsync(__dirname + "/data/data.json", "utf8")//need a data dir and data.json
+  .then(function(contents){
+    var msgObject = JSON.parse(contents)
+    return request.get(msgObject.text)//?
+      .endAsync()
+  })
+  .then(function(res){
+    var msgs = parseToObject(extractMsgs(res.text)) //extractMsgs - function to get file contents
+    return fs.writeFileAsync(__dirname + "/data/links.json", JSON.stringify(msgs), 'utf8')
+  })
+  .catch(function(error){
+    console.log(error)
+  })
 
+
+// console.log("welcome to goe")
+
+// var h = require('hyperscript');
+// var $ = require("jquery");
+
+
+// window.setInterval(refreshMsg, 5000)
+// function refreshMsg(){
+//   $('div.message').remove()
+//   console.log("Time : ")
+//   getMessages()
+// }
+
+// function getMessages() {
+//   $.get("http://localhost:3000/v1/messages")
+//   .done(function(data){
+//     oldMessages = data.messages.length;
+//     for (var i = 0; i < data.messages.length; i++){
+//       console.log("data : ", data.messages +" : " + "old" + oldMessages)
+//       var sender = data.messages[i].sender;
+//       var message = data.messages[i].message;
+//       var html = h('div.message', {style: {'font-size': '1.5em'}}, h('p', {}, sender + ": " + message), h('hr',{}))
+//       $('main').append(html)
+//     }
+//   })
+//   .fail(function(err){
+//     console.log(err);
+//   })
+// }
+
+// $(document).ready(function(){
+//   $('form').submit(function(event){
+//     var newMessage = {}
+//     newMessage["sender"] = $('input#name').val()
+//     newMessage["message"] = $('textarea#message').val() //this was input#message
+//     $.post("http://localhost:3000/v1/messages", newMessage)// function(){
+//     // })
+//     // .done(function() {
+//     //   console.log('new message sent!')
+//     // })
+//     // .fail(function() {
+//     //   console.log( 'error' );
+//     // })
+// })
+// })
