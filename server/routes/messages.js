da43b15d04c9ae7d9a35ed41d7400a8ef0a4b5b0
lou <email": null>
2016-03-28 16:18:49 +1300

    Add new empty files

---
+++ server/routes/messages.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+messages = {
+  messages: [
+    {
+      "id": 1,
+      "sender": "Winter",
+      "message": "Ice still grips you spring"
+    },
+   ]
+}
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json(messages);
+});
+
+router.post('/', function(req, res, next) {
+  var newMessage = req.body
+  if (validate(newMessage) == false){
+    res.status(400).send('String values required in all fields')
+  }
+  newMessage["id"] = messages.messages.length + 1
+  messages.messages.push(newMessage)
+  res.json(messages);
+});
+
+function validate(message) {
+  for (var property in message) {
+    if (typeof message[property] != 'string' || message[property] == '') {
+      return false
+    }
+  }
+  return true
+}
+
+module.exports = router;
