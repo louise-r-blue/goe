488348d2e81353d92e37f3612500bd894dcd893b
lou <email": null>
2016-03-30 22:01:59 +1300

    Try sav a msg to db

--- index.js
+++ index.js
@@ -6,30 +6,17 @@ var Promise = require('bluebird')
 var fs = Promise.promisifyAll(require('fs'))
 var request = require('superagent')
 
-// window.setInterval(refreshMsg, 5000)
-// function refreshMsg(){
-//   $('div.message').remove()
-//   console.log("Time : " )
-//   getMessages()
-// }
-// function testEndPoint(){
-//   request
-//     .get("http://localhost:3000/v1/messages")
-//     .end(function(error,result){
-//       console.log("result :", result)
-//       console.log("hi: " ,request)
-//     });
-// }
+
 
 function testEndPoint(){
   $.get("http://localhost:3000/v1/messages")
     .done(function(data,status){
-      console.log("result :", data)
-      //oldMessages = data.messages.length;
-      for( var i = 0; i < data.messages.length; i++){
+      //console.log("result :", data.length)
+      for( var i = 0; i < data.length; i++){
         var user_name = data[i].user_name;
         var message = data[i].messages;
-        var html = h('div.message', {style: {'font-size': '1.3em'}}, h('p', {}, user_name + ": " + message), h('hr',{}))
+        console.log("user", user_name, message)
+        var html = h('div.message', {style: {'font-size': '1.1em'}}, h('p', {}, user_name + ": " + message), h('hr',{}))
         $('main').append(html)
     }
   })
@@ -37,23 +24,7 @@ function testEndPoint(){
     console.log(err);
   })
 }
-// function getMessages() {
-//   $.get("http://localhost:3000/v1/messages")
-//   .done(function(data){
-//     console.log("client", data)
-//     oldMessages = data.messages.length;
-//     for (var i = 0; i < data.messages.length; i++){
-//       //console.log("data : ", data.messages +" : " + "old" + oldMessages)
-//       var sender = data.messages[i].sender;
-//       var message = data.messages[i].message;
-//       var html = h('div.message', {style: {'font-size': '1.5em'}}, h('p', {}, sender + ": " + message), h('hr',{}))
-//       $('main').append(html)
-//     }
-//   })
-//   .fail(function(err){
-//     console.log(err);
-//   })
-// }
+
 
 $(document).ready(function(){
   testEndPoint()
