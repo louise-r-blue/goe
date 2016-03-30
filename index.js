d2b8c202ddc2837945100d0478c3586a89bc0179
lou <email": null>
2016-03-30 20:33:53 +1300

    Update client-side

--- index.js
+++ index.js
@@ -12,64 +12,38 @@ var request = require('superagent')
 //   console.log("Time : " )
 //   getMessages()
 // }
+// function testEndPoint(){
+//   request
+//     .get("http://localhost:3000/v1/messages")
+//     .end(function(error,result){
+//       console.log("result :", result)
+//       console.log("hi: " ,request)
+//     });
+// }
 
-function getMessages() {
+function testEndPoint(){
   $.get("http://localhost:3000/v1/messages")
-  .done(function(data){
-    oldMessages = data.messages.length;
-    for (var i = 0; i < data.messages.length; i++){
-      //console.log("data : ", data.messages +" : " + "old" + oldMessages)
-      var sender = data.messages[i].sender;
-      var message = data.messages[i].message;
-      var html = h('div.message', {style: {'font-size': '1.5em'}}, h('p', {}, sender + ": " + message), h('hr',{}))
-      $('main').append(html)
+    .done(function(data,status){
+      console.log("result :", data)
+      //oldMessages = data.messages.length;
+      for( var i = 0; i < data.messages.length; i++){
+        var user_name = data[i].user_name;
+        var message = data[i].messages;
+        var html = h('div.message', {style: {'font-size': '1.3em'}}, h('p', {}, user_name + ": " + message), h('hr',{}))
+        $('main').append(html)
     }
   })
   .fail(function(err){
     console.log(err);
   })
 }
-
-$(document).ready(function(){
-  getMessages()
-  $('form').submit(function(event){
-    var newMessage = {}
-    newMessage["sender"] = $('input#name').val()
-    newMessage["message"] = $('textarea#message').val()
-    //this was input#message maybe here need to push new message onto messages array
-    $.post("http://localhost:3000/v1/messages", newMessage)//this posts to server
-})
-})
-
-// $(document).ready(function(){
-//   $('form').submit(function(event){
-//     var newMessage = {}
-//     newMessage["sender"] = $('input#name').val()
-//     newMessage["message"] = $('textarea#message').val() //this was input#message
-//     // maybe here need to push new message onto messages array
-//     $.post("http://localhost:3000/v1/messages", newMessage)//this posts to server
-// })
-// })
-
-// console.log("welcome to goe")
-
-// var h = require('hyperscript');
-// var $ = require("jquery");
-
-
-// window.setInterval(refreshMsg, 5000)
-// function refreshMsg(){
-//   $('div.message').remove()
-//   console.log("Time : ")
-//   getMessages()
-// }
-
 // function getMessages() {
 //   $.get("http://localhost:3000/v1/messages")
 //   .done(function(data){
+//     console.log("client", data)
 //     oldMessages = data.messages.length;
 //     for (var i = 0; i < data.messages.length; i++){
-//       console.log("data : ", data.messages +" : " + "old" + oldMessages)
+//       //console.log("data : ", data.messages +" : " + "old" + oldMessages)
 //       var sender = data.messages[i].sender;
 //       var message = data.messages[i].message;
 //       var html = h('div.message', {style: {'font-size': '1.5em'}}, h('p', {}, sender + ": " + message), h('hr',{}))
@@ -81,18 +55,14 @@ $(document).ready(function(){
 //   })
 // }
 
-// $(document).ready(function(){
-//   $('form').submit(function(event){
-//     var newMessage = {}
-//     newMessage["sender"] = $('input#name').val()
-//     newMessage["message"] = $('textarea#message').val() //this was input#message
-//     $.post("http://localhost:3000/v1/messages", newMessage)// function(){
-//     // })
-//     // .done(function() {
-//     //   console.log('new message sent!')
-//     // })
-//     // .fail(function() {
-//     //   console.log( 'error' );
-//     // })
-// })
-// })
+$(document).ready(function(){
+  testEndPoint()
+  //getMessages()
+  $('form').submit(function(event){
+    var newMessage = {}
+    newMessage["sender"] = $('input#name').val()
+    newMessage["message"] = $('textarea#message').val()
+    //this was input#message maybe here need to push new message onto messages array
+    $.post("http://localhost:3000/v1/messages", newMessage)//this posts to server
+})
+})
